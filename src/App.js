a8f51f310aa0ce22d19ab9bbe03e6c0aafdc8b7d
mert <smilinggreenfrog31@gmail.com>
2022-12-24 19:48:05 +0300

    React-Redux config & Alert init

--- src/App.js
+++ src/App.js
@@ -5,20 +5,27 @@ import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import './App.css';
+import Alert from './components/layout/Alert';
+//Redux
+import { Provider } from 'react-redux';
+import store from './store';
 
 const App = () => (
-  <BrowserRouter>
-    <Fragment>
-      <Navbar />
-      <section className='container'>
-        <Routes>
-          <Route path='/' element={<Landing />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/register' element={<Register />} />
-        </Routes>
-      </section>
-    </Fragment>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <Fragment>
+        <Navbar />
+        <section className='container'>
+          <Alert />
+          <Routes>
+            <Route path='/' element={<Landing />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+          </Routes>
+        </section>
+      </Fragment>
+    </BrowserRouter>
+  </Provider>
 );
 
 export default App;
--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,8 +1,11 @@
 import React, { Fragment, useState } from 'react';
 import axios from '../../axios';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { setAlert } from '../../actions/alert';
+import PropTypes from 'prop-types';
 
-const Register = () => {
+const Register = ({ setAlert }) => {
   const [data, setData] = useState({
     name: '',
     email: '',
@@ -18,7 +21,7 @@ const Register = () => {
   const clickSubmit = async (e) => {
     e.preventDefault();
     if (password !== password2) {
-      console.log('passwords do not match');
+      setAlert('Passwords do not match','danger');
     } else {
       const newUser = {
         name,
@@ -103,4 +106,9 @@ const Register = () => {
   );
 };
 
-export default Register;
+
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired,
+};
+
+export default connect(null,{ setAlert })(Register);
