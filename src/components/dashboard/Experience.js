34b2e38478149ae8963c1fdf9fdced74225c1b56
Huseyin <huseyinkarakaya006@gmail.com>
2022-12-30 18:04:19 +0300

    Dasboard bug fixed

--- src/components/dashboard/Experience.js
+++ src/components/dashboard/Experience.js
@@ -1,20 +1,20 @@
 import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { deleteEducation } from '../../actions/profile';
+import { deleteExperience } from '../../actions/profile';
 import formatDate from '../../utils/formatDate';
 
-const Education = ({ education, deleteEducation }) => {
-  const educations = education.map((edu) => (
-    <tr key={edu._id}>
-      <td>{edu.school}</td>
-      <td className='hide-sm'>{edu.degree}</td>
+const Experience = ({ experience, deleteExperience }) => {
+  const experiences = experience.map((exp) => (
+    <tr key={exp._id}>
+      <td>{exp.company}</td>
+      <td className='hide-sm'>{exp.title}</td>
       <td>
-        {formatDate(edu.from)} - {edu.to ? formatDate(edu.to) : 'Now'}
+        {formatDate(exp.from)} - {exp.to ? formatDate(exp.to) : 'Now'}
       </td>
       <td>
         <button
-          onClick={() => deleteEducation(edu._id)}
+          onClick={() => deleteExperience(exp._id)}
           className='btn btn-danger'
         >
           Delete
@@ -25,25 +25,25 @@ const Education = ({ education, deleteEducation }) => {
 
   return (
     <Fragment>
-      <h2 className='my-2'>Education Credentials</h2>
+      <h2 className='my-2'>Experience Credentials</h2>
       <table className='table'>
         <thead>
           <tr>
-            <th>School</th>
-            <th className='hide-sm'>Degree</th>
+            <th>Company</th>
+            <th className='hide-sm'>Title</th>
             <th className='hide-sm'>Years</th>
             <th />
           </tr>
         </thead>
-        <tbody>{educations}</tbody>
+        <tbody>{experiences}</tbody>
       </table>
     </Fragment>
   );
 };
 
-Education.propTypes = {
-  education: PropTypes.array.isRequired,
-  deleteEducation: PropTypes.func.isRequired,
+Experience.propTypes = {
+  experience: PropTypes.array.isRequired,
+  deleteExperience: PropTypes.func.isRequired,
 };
 
-export default connect(null, { deleteEducation })(Education);
+export default connect(null, { deleteExperience })(Experience);
