f1c6ef33f97cf579345179e32138ddc3bd4c21bd
mert <smilinggreenfrog31@gmail.com>
2023-03-12 14:32:30 +0300

    Yanlışlıkla question a ait kodları silmiştim ama bu branchte yani test branchinde onları ana branchten ekledim

---
+++ src/components/question/CommentForm.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { addComment } from '../../actions/question';
+
+const CommentForm = ({ questionId, addComment }) => {
+  const [description, setDescription] = useState('');
+
+  
+  return (
+    <div className='post-form'>
+      <div className='bg-primary p'>
+        <h3>Leave a Comment</h3>
+      </div>
+      <form
+        className='form my-1'
+        onSubmit={(e) => {
+          e.preventDefault();
+          addComment(questionId, { description });
+          setDescription('');
+        }}
+      >
+        <textarea
+          name='text'
+          cols='30'
+          rows='5'
+          placeholder='Answer the question'
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          required
+        />
+        <input type='submit' className='btn btn-dark my-1' value='Submit' />
+      </form>
+    </div>
+  );
+};
+
+CommentForm.propTypes = {
+  addComment: PropTypes.func.isRequired,
+};
+
+export default connect(null, { addComment })(CommentForm);
