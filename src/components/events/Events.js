543f746284bac50ee33444039b8b823ee70a8f7c
mert <smilinggreenfrog31@gmail.com>
2023-03-12 15:34:59 +0300

    Daha yeni,checkbox işaretlendiğinde pop up çıkıcak ve onclick checkbox ta fonksiyon çalışacak

--- src/components/events/Events.js
+++ src/components/events/Events.js
@@ -13,6 +13,49 @@ const Questions = ({ getQuestions, question: { questions } }) => {
       <h1 className='large text-primary'>Events</h1>
       <p className='lead'>
         <i className='fas fa-user' /> Welcome to the events page
+      
+        
+        <img src="https://images-prod.dazeddigital.com/640/0-58-1080-720/azure/dazed-prod/1320/8/1328021.jpg" 
+        height="100" max width="%50"
+        
+        
+        
+
+        alt="Lady Gaga Concert"></img>
+
+        <input type='submit' className='btn btn-dark my-1' value='100€' /*onClick={}*/ />
+        <input type='button' className='btn btn-dark my-1' value='Lady Gaga Concert' /*onClick={}*/ />
+        <input type='submit' className='btn btn-dark my-1' value='Istanbul-IF Performance Hall' /*onClick={}*/ />
+        
+        <input type="checkbox" className='btn btn-dark my-1' />
+
+        <img src="https://images-prod.dazeddigital.com/640/0-58-1080-720/azure/dazed-prod/1320/8/1328021.jpg" 
+        height="100" max width="%50"
+        
+        
+        
+
+        alt="Lady Gaga Concert"></img>
+
+        <input type='submit' className='btn btn-dark my-1' value='100€' /*onClick={}*/ />
+        <input type='button' className='btn btn-dark my-1' value='Lady Gaga Concert' /*onClick={}*/ />
+        <input type='submit' className='btn btn-dark my-1' value='Istanbul-IF Performance Hall' /*onClick={}*/ />
+        
+        <input type="checkbox" className='btn btn-dark my-1' />
+
+        <img src="https://images-prod.dazeddigital.com/640/0-58-1080-720/azure/dazed-prod/1320/8/1328021.jpg" 
+        height="100" max width="%50"
+        
+        
+        
+
+        alt="Lady Gaga Concert"></img>
+
+        <input type='submit' className='btn btn-dark my-1' value='100€' /*onClick={}*/ />
+        <input type='button' className='btn btn-dark my-1' value='Lady Gaga Concert' /*onClick={}*/ />
+        <input type='submit' className='btn btn-dark my-1' value='Istanbul-IF Performance Hall' /*onClick={}*/ />
+        
+        <input type="checkbox" className='btn btn-dark my-1' />
       </p>
       
       
