8ebec25f3c191c1d9ab3f341925aeeaeaee7dbe8
mert <smilinggreenfrog31@gmail.com>
2023-03-17 14:40:45 +0300

    Daha eklenecek birsürü şey var

--- src/components/events/Events.js
+++ src/components/events/Events.js
@@ -9,53 +9,45 @@ const Questions = ({ getQuestions, question: { questions } }) => {
   }, [getQuestions]);
 
   return (
-    <section className='container'>
+    <section className='eventcontainer'>
       <h1 className='large text-primary'>Events</h1>
       <p className='lead'>
-        <i className='fas fa-user' /> Welcome to the events page
-      
-        
+        <i className='fas fa-user' /> Welcome to the events page 
+        <p className='p.a'/>
+        <p className='p.a'/>
+        <p className='p.a'/>
         <img src="https://images-prod.dazeddigital.com/640/0-58-1080-720/azure/dazed-prod/1320/8/1328021.jpg" 
-        height="100" max width="%50"
-        
-        
-        
+        height="100" max width="%75"
+        alt=""></img >
 
+        <input type='submit' className='btn2' value='Details' /*onClick={}*/ />
+        <input type='submit' className='btn2' value='Attend' /*onClick={}*/ />
+
+
+        <img src="https://assets.dicebreaker.com/chess-playing-hand.jpeg/BROK/thumbnail/1600x900/quality/100/chess-playing-hand.jpeg" 
+        height="100" max width="%50"
         alt="Lady Gaga Concert"></img>
 
-        <input type='submit' className='btn btn-dark my-1' value='100€' /*onClick={}*/ />
-        <input type='button' className='btn btn-dark my-1' value='Lady Gaga Concert' /*onClick={}*/ />
-        <input type='submit' className='btn btn-dark my-1' value='Istanbul-IF Performance Hall' /*onClick={}*/ />
+        <input type='submit' className='btn2' value='Details' /*onClick={}*/ />
+        <input type='submit' className='btn2' value='Attend' /*onClick={}*/ />
         
-        <input type="checkbox" className='btn btn-dark my-1' />
+     
 
-        <img src="https://images-prod.dazeddigital.com/640/0-58-1080-720/azure/dazed-prod/1320/8/1328021.jpg" 
+        <img src="https://gaiadergi.com/wp-content/uploads/2016/01/Agac-dikimi-isine-girmek-3.jpg" 
         height="100" max width="%50"
-        
-        
-        
-
         alt="Lady Gaga Concert"></img>
 
-        <input type='submit' className='btn btn-dark my-1' value='100€' /*onClick={}*/ />
-        <input type='button' className='btn btn-dark my-1' value='Lady Gaga Concert' /*onClick={}*/ />
-        <input type='submit' className='btn btn-dark my-1' value='Istanbul-IF Performance Hall' /*onClick={}*/ />
-        
-        <input type="checkbox" className='btn btn-dark my-1' />
+        <input type='submit' className='btn2' value='Details' /*onClick={}*/ />
+        <input type='submit' className='btn2' value='Attend' /*onClick={}*/ />
 
-        <img src="https://images-prod.dazeddigital.com/640/0-58-1080-720/azure/dazed-prod/1320/8/1328021.jpg" 
+        <img src="https://www.ucf.edu/news/files/2022/03/game-design.jpg" 
         height="100" max width="%50"
-        
-        
-        
-
         alt="Lady Gaga Concert"></img>
 
-        <input type='submit' className='btn btn-dark my-1' value='100€' /*onClick={}*/ />
-        <input type='button' className='btn btn-dark my-1' value='Lady Gaga Concert' /*onClick={}*/ />
-        <input type='submit' className='btn btn-dark my-1' value='Istanbul-IF Performance Hall' /*onClick={}*/ />
-        
-        <input type="checkbox" className='btn btn-dark my-1' />
+        <input type='submit' className='btn2' value='Details' /*onClick={}*/ />
+        <input type='submit' className='btn2' value='Attend' /*onClick={}*/ />
+
+     
       </p>
       
       
