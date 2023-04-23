aeab395dd5f0cd1c3f4b927dfd51f282fa40d852
Huseyin <huseyinkarakaya006@gmail.com>
2023-04-23 14:35:35 +0300

    landing updated

--- src/components/layout/Landing.js
+++ src/components/layout/Landing.js
@@ -4,45 +4,50 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 /*import axios from 'axios';*/
 
-const Landing = ({ isAuthenticated }) => {
-  if (isAuthenticated) {
-     /*const response = axios.get('/auth');
-    if(response.data === "company")
-    return <Navigate to='/dashboardCompany' />;
-    if(response.data === "user")*/
+const Landing = ({ isAuthenticated, role }) => {
+  if (isAuthenticated && role === 'engineer') {
+    // console.log('LOG: Role:', role);
     return <Navigate to='/dashboard' />;
   }
+
+  if (isAuthenticated && role === 'company') {
+    // console.log('LOG: Role:', role);
+    return <Navigate to='/dashboardCompany' />;
+  }
+
   return (
     <section className='landing'>
-        <div className='landing-inner'>
-          <h1 className='x-large'>C Cube</h1>
-          <p className='lead'>Connect-Communicate-Collaborate</p>
-          <p className='lead'>
-            Create your profile, share posts<br/> and get help from
-            other engineers
-          </p><br/>
-          <div className='buttons'>
-            <Link to='/login' className='btn btn-light'>
-              Login
-            </Link>
-            <Link to='/register' className='btn btn-primary'>
-              Sign Up
-            </Link>
-            <Link to='/registerComp' className='btn btn-primary'>
-              Sign Up For Company
-            </Link>
-          </div>
+      <div className='landing-inner'>
+        <h1 className='x-large'>C Cube</h1>
+        <p className='lead'>Connect-Communicate-Collaborate</p>
+        <p className='lead'>
+          Create your profile, share posts
+          <br /> and get help from other engineers
+        </p>
+        <br />
+        <div className='buttons'>
+          <Link to='/login' className='btn btn-light'>
+            Login
+          </Link>
+          <Link to='/register' className='btn btn-primary'>
+            Sign Up
+          </Link>
+          <Link to='/registerComp' className='btn btn-primary'>
+            Sign Up For Company
+          </Link>
         </div>
-
+      </div>
     </section>
   );
 };
 
 Landing.propTypes = {
   isAuthenticated: PropTypes.bool,
+  role: PropTypes.string,
 };
 const mapStateToProps = (state) => ({
   isAuthenticated: state.auth.isAuthenticated,
+  role: state.auth.role,
 });
 
 export default connect(mapStateToProps)(Landing);
